195835293a7a55baa87559216843580f947580e2
Rexford <rexfordocansey@gmail.com>
2026-04-22 03:13:51 +0100

    improving security

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,13 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^myr+uhgf2f=a(rzsl9xjhqa(t23@_k_x#^psd*m^#7n1b5xg@'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-^myr+uhgf2f=a(rzsl9xjhqa(t23@_k_x#^psd*m^#7n1b5xg@')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',') if os.environ.get('DJANGO_ALLOWED_HOSTS') else []
 
 
 # Application definition

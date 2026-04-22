d60e873fab3bcb216dc44c3eb85c3a5dbdc53ac7
Rexford <rexfordocansey@gmail.com>
2026-04-22 04:33:43 +0100

    connecting the frontend to the backend

--- config/urls.py
+++ config/urls.py
@@ -1,26 +1,19 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from accounts.test_views import login_test
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from accounts.views import LogoutRedirectView
+from accounts.test_views import login_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/profiles/', include('profiles.urls')),
-    path('', login_test, name='login-test'),
-]
+    path('', login_page, name='login'),
+    path('login', login_page, name='login-alt'),
+    path('homepage', TemplateView.as_view(template_name='homepage.html'), name='homepage'),
+    path('profile', TemplateView.as_view(template_name='profile.html'), name='profile'),
+    path('support', TemplateView.as_view(template_name='support.html'), name='support'),
+    path('logout', LogoutRedirectView.as_view(), name='logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

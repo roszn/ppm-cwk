b56e6343ba29c2e3357cdd612b0ab23b3a8d0ee3
Rexford <rexfordocansey@gmail.com>
2026-04-23 18:38:25 +0100

    changes

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 
+@ensure_csrf_cookie
 def login_page(request):
     return render(request, 'login_test.html')
